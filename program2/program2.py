ab7f13a8845a847b745185d1d20399cfe354484a
Rafaello <rafjar@Rafalek>
2019-12-26 20:29:04 +0100

    Działające symulacje

--- program2/program2.py
+++ program2/program2.py
@@ -1,53 +1,60 @@
-from random import random, randint #losowa liczba z zakresu <0,1), losowa liczba z zakresu <a,b>
-from sys import argv #stare dobre argv
 import numpy as np
+from random import randint, random
+from sys import argv
 import math
-#from scipy.special import comb #symbol newtona
-#import matplotlib.pyplot as plt
 
 N = 100
-p = 0.9
+p = 0.1
 if len(argv) == 3:
-    N, p = int(argv[1]), float(argv[2])
+    N, p = argv[1], argv[2]
+
+matrix = np.zeros((N, N), dtype = int) #utworzenie macierzy NxN pełnej zer
+theta = math.log(p / (1-p)) #theta ze wzorku z hamiltonianu
+E_current = 0 # aktualna liczba krawędzi grafu
+how_many_iterations = N**3 # liczba iteracji (losowań grafu i jego zmiany)
+
+## pomocniczne funkcje (używane niżej) ##
 
 def random_vertex():
     row, col = randint(0, N-1), randint(0, N-1)
     while row == col:
-        col = randint(0, N-1)
+        col = randint(0, N-1) #losowanie col != row (żeby nie wylosować przekątnej)
     return (row, col)
 
-matrix = np.zeros((N, N), dtype = int) #macierz pełna zer
-theta = math.log((1-p) / p)
-edges = 0 #liczba krawędzi
-how_many_iterations = N**3
+def add_vertex(row, col): #dodaje krawędź we wskazanym miejscu
+    global E_current
+    matrix[row][col], matrix[col][row] = 1, 1
+    E_current += 1
 
-for iteracja in range(how_many_iterations):
-    row, col = random_vertex()
-    
-    H_current = -theta * edges
-    H_new = 0
+def remove_vertex(row, col): #usuwa krawędź we skazanym miejscu
+    global E_current
+    matrix[row][col], matrix[col][row] = 0, 0
+    E_current -= 1
 
-    if matrix[row][col] == 0:
-        H_new = -theta * (edges+1)
-    else:
-        H_new = -theta * (edges-1)
-    dH = H_new - H_current
-    if dH < 0:
-        if matrix[row][col] == 0:
-            matrix[row][col], matrix[col][row] = 1, 1
-            edges += 1
-        else:
-            matrix[row][col], matrix[col][row] = 0, 0
-            edges -= 1
-    else:
-        rand = random()
-        if rand < math.exp(dH):
-            if matrix[row][col] == 0:
-                matrix[row][col], matrix[col][row] = 1, 1
-                edges += 1
-            else:
-                matrix[row][col], matrix[col][row] = 0, 0
-                edges -= 1
+#########################################
 
-print(edges)
+## główna część programu ##
+for iteration in range (how_many_iterations):
+    row, col = random_vertex() #losowanie węzła spoza przekątnej
+
+    H_current = theta * E_current #obecny hamiltonian Hi=theta*E_i
+
+    if matrix[row][col] == 0: #przypadek gdy wylosowaliśmy brak krawędzi (rozpatrujemy jej dodanie)
+        H_new = theta * (E_current + 1) #nowy hamiltonian Hj=theta*E_j
+
+        dH = H_new - H_current #delta hamiltonianów
+        
+        if random() < math.exp(dH): #z prawdopodobieństwem dodajemy krawędź
+            add_vertex(row, col)
+
+    else: #przypadek gdy wylosowaliśmy krawędź (rozpatrujemy jej usunięcie)
+        H_new = theta * (E_current - 1) #nowy hamiltonian Hj=theta*E_j
+
+        dH = H_new - H_current
+
+        if random() < math.exp(dH): #z prawdopodobieństwem dodajemy krawędź
+            remove_vertex(row, col)
+
+    print("Edges", E_current)
+
 np.savetxt("macierz_" + str(N) + "x" + str(N) + "_" + str(p), matrix, fmt = '%.1d')
