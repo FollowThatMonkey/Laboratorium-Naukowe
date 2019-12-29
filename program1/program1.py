1ffb0ba0bb9c1b6fb132438a48dc896cba059381
Rafaello <rafjar@Rafalek>
2019-12-29 21:42:28 +0100

    Małe poprawki

--- program1/program1.py
+++ program1/program1.py
@@ -9,26 +9,29 @@ p = 0.1 #prawdopodobieństwo krawędzi
 if len(sys.argv) == 3: #jeśli w argumencie wywołania są dane
     N, p = int(sys.argv[1]), float(sys.argv[2]) #to program zostanie wywołany z nimi, 1 argument to N, 2 argument to p
 
-def random_graph(N, p):
-    matrix = np.zeros((N, N), dtype = int)
+#########################################
+
+## pomocniczne funkcje ##
+
+def random_graph(N, p): # tworzenie losowego grafu klasyczną metodą
+    matrix = np.zeros((N, N), dtype = int) # tworzenie macierzy pełnej zer o rozmiarze NxN
 
     # tworzenie macierzy sąsiedztwa
     for row in range(N):
         for col in range(row + 1, N):
-            rand = random()
-            if rand < p:
+            if random() < p:
                 matrix[col][row], matrix[row][col] = 1, 1
     
-    np.savetxt("macierz_" + str(N) + "x" + str(N) + "_" + str(p), matrix, fmt = '%.1d')
+    np.savetxt("macierz_" + str(N) + "x" + str(N) + "_" + str(p), matrix, fmt = '%.1d') # zapis macierzy do pliku
 
-    ki = np.zeros(N, dtype = int) #tablica stopni wierzchołków
+    ki = np.zeros(N, dtype = int) #tablica stopni wierzchołków, z niej dalej będzie tworzony histogram P(k)
     for row in range(N):
         for col in range(N):
             ki[row] += matrix[row][col]
 
     return ki
 
-def binominal_dist(N, p):
+def binominal_dist(N, p): # P(k) ze wzoru
     P = np.zeros(N)
 
     for ki in range(N):
@@ -36,20 +39,27 @@ def binominal_dist(N, p):
 
     return P
 
+#########################################
+
+## wywołanie powyższych funkcji ##
 
 praktyka = random_graph(N, p)
 teoria = binominal_dist(N, p)
 
-# Wykresik
+#########################################
+
+## Wykresik ##
 num_bins = (np.amax(praktyka)-np.amin(praktyka)) - 1
 
-plt.hist(praktyka, bins = num_bins, density = True, rwidth = 0.9)
-plt.bar(np.arange(len(teoria)), teoria, color = 'y', width = 0.7, alpha = 0.5)
+plt.hist(praktyka, bins = num_bins, density = True, rwidth = 0.9) # utworzenie znormalizowanego histogramu dla klasycznego grafu losowego
+plt.bar(np.arange(len(teoria)), teoria, color = 'y', width = 0.7, alpha = 0.5) # utworzenie wykresu słupkowego P(k) dla wzorku teoretycznego
 plt.title("Histogram P(k) dla wygenerowanego grafu losowego o N = " + str(N) + " oraz p = " + str(p) + " oraz danych teoretycznych", wrap=True)
 plt.xlabel("k")
 plt.ylabel("Prawdopodobieństwo")
 plt.xlim(np.amin(praktyka) - int(math.sqrt(N/10)), np.amax(praktyka) + int(math.sqrt(N/10)))
 plt.grid(True)
 
-plt.savefig("histogram_" + str(N) + "x" + str(N) + "_" + str(p) + ".png", dpi = 240)
+plt.savefig("histogram_" + str(N) + "x" + str(N) + "_" + str(p) + ".png", dpi = 240) # zapisanie wykresu do pliku
 plt.show()
+
+#########################################
