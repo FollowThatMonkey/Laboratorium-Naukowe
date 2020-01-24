ac77ee431f6faf083bf05a7ffc5b3b2630b553dc
Rafaello <rafjar@Rafalek>
2020-01-24 11:20:15 +0100

    Gotowy program porównujący x ostatnich średnich z dokładnością do pewnej liczby

--- program2/program2.py
+++ program2/program2.py
@@ -4,6 +4,7 @@ from sys import argv
 import math
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from statistics import mean
 
 N = 100
 p = 0.1
@@ -13,7 +14,12 @@ if len(argv) == 3:
 matrix = np.zeros((N, N), dtype = int) #utworzenie macierzy NxN pełnej zer
 theta = math.log(p / (1-p)) #theta ze wzorku z hamiltonianu
 E_current = 0 # aktualna liczba krawędzi grafu
-how_many_iterations = int(1.5e6) # liczba iteracji (losowań grafu i jego zmiany)
+how_many_iter = 800 # liczba iteracji co ile będzie sprawdzane czy <E>=const.
+E_mean_list = [0] * how_many_iter #tablica lizcby krawędzi grafu z ostatnich iteracji
+E_mean_current = 0 #obecna średnia liczba krawędzi
+how_many_previous = 10 #liczba mówiąca ile ostatnich średnich będzie porównywana
+E_mean_previous = [math.nan]*how_many_previous #tablica zawierająca określoną liczbę ostatnio liczonych średnich
+comparison_accuracy = 15 #dokładność z którą porównywane będą średnie
 
 ## pomocniczne funkcje (używane niżej) ##
 
@@ -36,7 +42,9 @@ def remove_vertex(row, col): #usuwa krawędź we wskazanym miejscu
 #########################################
 
 ## główna część programu ##
-for iteration in range (how_many_iterations):
+iteration = 0
+mean_iteration = 0 #iterator potrzebny do dodawania poprzednich średnich na odpowiednie miejsce w tablicy E_mean_previous
+while True:
     row, col = random_vertex() #losowanie węzła spoza przekątnej
 
     H_current = theta * E_current #obecny hamiltonian Hi=theta*E_i
@@ -52,6 +60,21 @@ for iteration in range (how_many_iterations):
             remove_vertex(row, col)
         elif random() <= math.exp(-theta):
             remove_vertex(row, col)
+    
+    E_mean_list[iteration % how_many_iter] = E_current
+
+    if iteration % how_many_iter == 0:
+        E_mean_current = int(mean(E_mean_list))
+
+        print("Iteracja =", iteration, ", E_mean_current =", E_mean_current,", E_mean_previous =", E_mean_previous)
+
+        if all(abs(E_mean_current - prev_mean) <= comparison_accuracy for prev_mean in E_mean_previous): # jeśli wszystkie średnie z tablicy są równe aktualnej średniej z dokładnością do comparison_accuracy, to break
+            break
+        else: #jeśli średnie z dokładnoscią do pewnej liczby się różnią, to:
+            E_mean_previous[mean_iteration%how_many_previous] = E_mean_current #dodanie aktualnej średniej do tablicy poprzednich średnich
+            mean_iteration += 1 #zwiększenie iteratora mówiącego którą średnią z poprzednich (z tablicy) teraz będziemy rozpatrywać
+   
+    iteration += 1
 
 #########################################
 
